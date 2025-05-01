30900b331ee6d8219bb6f7b5ec99d83344c1aece
Sic0 <lucianomarzoratti@gmail.com>
2025-05-01 01:50:08 -0300

    v1.0.0

--- renderer/preload.d.ts
+++ renderer/preload.d.ts
@@ -2,6 +2,12 @@ import { IpcHandler } from '../main/preload'
 
 declare global {
   interface Window {
-    ipc: IpcHandler
+    ipc: IpcHandler,
+    electronAPI: {
+      selectPesFolder: () => Promise<{ error?: string; fileExists?: boolean; selected?: string }>;
+      setSelectedLeague?: (leagueName: string) => Promise<{ error?: string }>;
+    };
   }
 }
+
+export { }
